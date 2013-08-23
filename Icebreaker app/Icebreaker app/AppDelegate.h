84259c65765500f36e4ef73616070586b714fef8
ewu4 <evannwu4@gmail.com>
2013-08-23 16:45:00 -0400

    Icebreaker Xcode

---
+++ Icebreaker app/Icebreaker app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Icebreaker app
+//
+//  Created by Ethan Wu on 8/23/13.
+//  Copyright (c) 2013 Ethan Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Icebreaker app/Icebreaker app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Icebreaker app
+//
+//  Created by Ethan Wu on 8/23/13.
+//  Copyright (c) 2013 Ethan Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
